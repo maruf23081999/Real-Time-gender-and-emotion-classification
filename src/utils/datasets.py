ddc2c12d81f4f4ee8a4aefe5731bef71af50a8cc
maruf23081999 <48323634+maruf23081999@users.noreply.github.com>
2020-01-27 13:13:19 +0530

    Add files via upload

---
+++ src/utils/datasets.py
@@ -0,0 +1,115 @@
+from scipy.io import loadmat
+import pandas as pd
+import numpy as np
+from random import shuffle
+import os
+import cv2
+
+
+class DataManager(object):
+    
+    def __init__(self, dataset_name='wiki',
+                 dataset_path=None, image_size=(48, 48)):
+
+        self.dataset_name = dataset_name
+        self.dataset_path = dataset_path
+        self.image_size = image_size
+        if self.dataset_path is not None:
+            self.dataset_path = dataset_path
+        elif self.dataset_name == 'wiki':
+            self.dataset_path = '../datasets/wiki_crop/wiki.mat'
+        elif self.dataset_name == 'fer2013':
+            self.dataset_path = '../datasets/fer2013.csv'
+        else:
+            raise Exception(
+                    'Incorrect dataset name, please input imdb or fer2013')
+
+    def get_data(self):
+        if self.dataset_name == 'wiki':
+            ground_truth_data = self._load_imdb()
+        elif self.dataset_name == 'fer2013':
+            ground_truth_data = self._load_fer2013()
+        return ground_truth_data
+
+    def _load_imdb(self):
+        face_score_treshold = 3
+        dataset = loadmat(self.dataset_path)
+        image_names_array = dataset['wiki']['full_path'][0, 0][0]
+        gender_classes = dataset['wiki']['gender'][0, 0][0]
+        face_score = dataset['wiki']['face_score'][0, 0][0]
+        second_face_score = dataset['wiki']['second_face_score'][0, 0][0]
+        face_score_mask = face_score > face_score_treshold
+        second_face_score_mask = np.isnan(second_face_score)
+        unknown_gender_mask = np.logical_not(np.isnan(gender_classes))
+        mask = np.logical_and(face_score_mask, second_face_score_mask)
+        mask = np.logical_and(mask, unknown_gender_mask)
+        image_names_array = image_names_array[mask]
+        gender_classes = gender_classes[mask].tolist()
+        image_names = []
+        for image_name_arg in range(image_names_array.shape[0]):
+            image_name = image_names_array[image_name_arg][0]
+            image_names.append(image_name)
+        return dict(zip(image_names, gender_classes))
+
+    def _load_fer2013(self):
+        data = pd.read_csv(self.dataset_path)
+        pixels = data['pixels'].tolist()
+        width, height = 48, 48
+        faces = []
+        for pixel_sequence in pixels:
+            face = [int(pixel) for pixel in pixel_sequence.split(' ')]
+            face = np.asarray(face).reshape(width, height)
+            face = cv2.resize(face.astype('uint8'), self.image_size)
+            faces.append(face.astype('float32'))
+        faces = np.asarray(faces)
+        faces = np.expand_dims(faces, -1)
+        emotions = pd.get_dummies(data['emotion']).as_matrix()
+        return faces, emotions
+
+
+
+def get_labels(dataset_name):
+    if dataset_name == 'fer2013':
+        return {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy',
+                4: 'sad', 5: 'surprise', 6: 'neutral'}
+    elif dataset_name == 'wiki':
+        return {0: 'woman', 1: 'man'}
+    elif dataset_name == 'KDEF':
+        return {0: 'AN', 1: 'DI', 2: 'AF', 3: 'HA', 4: 'SA', 5: 'SU', 6: 'NE'}
+    else:
+        raise Exception('Invalid dataset name')
+
+
+def get_class_to_arg(dataset_name='fer2013'):
+    if dataset_name == 'fer2013':
+        return {'angry': 0, 'disgust': 1, 'fear': 2, 'happy': 3, 'sad': 4,
+                'surprise': 5, 'neutral': 6}
+    elif dataset_name == 'wiki':
+        return {'woman': 0, 'man': 1}
+    elif dataset_name == 'KDEF':
+        return {'AN': 0, 'DI': 1, 'AF': 2, 'HA': 3, 'SA': 4, 'SU': 5, 'NE': 6}
+    else:
+        raise Exception('Invalid dataset name')
+
+
+def split_imdb_data(ground_truth_data, validation_split=.2, do_shuffle=False):
+    ground_truth_keys = sorted(ground_truth_data.keys())
+    if do_shuffle is not False:
+        shuffle(ground_truth_keys)
+    training_split = 1 - validation_split
+    num_train = int(training_split * len(ground_truth_keys))
+    train_keys = ground_truth_keys[:num_train]
+    validation_keys = ground_truth_keys[num_train:]
+    return train_keys, validation_keys
+
+
+def split_data(x, y, validation_split=.2):
+    num_samples = len(x)
+    num_train_samples = int((1 - validation_split)*num_samples)
+    train_x = x[:num_train_samples]
+    train_y = y[:num_train_samples]
+    val_x = x[num_train_samples:]
+    val_y = y[num_train_samples:]
+    train_data = (train_x, train_y)
+    val_data = (val_x, val_y)
+    return train_data, val_data
